a76df10dc967a3b030499ccc5a728751cdff2f47
SzluDora <szlucska.dora@gmail.com>
2023-02-23 19:02:47 +0100

    equalizer

--- Metodusok.java
+++ Metodusok.java
@@ -1,6 +1,6 @@
 package com.mycompany.metodusok;
 
-
+     
 public class Metodusok {
 
     public static void main(String[] args) {
@@ -17,6 +17,15 @@ public class Metodusok {
         int masodik = osszead(szam3, szam4);
         int eredmeny = osszead(elso, masodik);
         kiir("%d + %d + %d + %d = %d\n".formatted(szam1, szam2, szam3, szam4, eredmeny));
+        
+        int a = 3, b = 4, c = 2;
+        osszeg = osszead(a, b);
+        osszeg = osszead(osszeg, c);
+        String s = "%d +%d + %d gyöke: ".formatted(a, b, c);
+        double gyok = Math.sqrt(szam);
+        kimenet = String.format("%s %.4f\n", s, gyok);
+        kiir(kimenet);
+                
     }
     
     private static int elso10SzamOsszege() {
@@ -34,4 +43,12 @@ public class Metodusok {
     private static void kiir(String szoveg) {
         System.out.printf(szoveg);
     }
+    
+    private static void f1_ketszamosszege() {
+        int osszeg = elso10SzamOsszege();
+        String kimenet = "Az első 10 szám összege: " + osszeg + "\n";
+        kiir(kimenet);
+    }
+    
+    
 }
