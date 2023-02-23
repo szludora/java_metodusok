478ba57a31e6af82f947d3d4dc9335b4cdd8bee6
SzluDora <szlucska.dora@gmail.com>
2023-02-23 18:18:55 +0100

    masodik

--- Metodusok.java
+++ Metodusok.java
@@ -8,9 +8,15 @@ public class Metodusok {
         String kimenet = "Az első 10 szám összege: " + osszeg + "\n";
         kiir(kimenet);
         
-        int szam1 = 7, szam2 = 4;
-        osszeg = osszead(szam1, szam2);
-        kiir("%d + %d = %d\n".formatted(szam1, szam2, osszeg));
+        int szam = 7, masikszam = 4;
+        osszeg = osszead(szam, masikszam);
+        kiir("%d + %d = %d\n".formatted(szam, masikszam, osszeg));
+        
+        int szam1 = 4, szam2 = 3, szam3 = 6, szam4 = 9;
+        int elso = osszead(szam1, szam2);
+        int masodik = osszead(szam3, szam4);
+        int eredmeny = osszead(elso, masodik);
+        kiir("%d + %d + %d + %d = %d\n".formatted(szam1, szam2, szam3, szam4, eredmeny));
     }
     
     private static int elso10SzamOsszege() {
